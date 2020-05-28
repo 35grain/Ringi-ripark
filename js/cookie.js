b29877f0d0365a7c5c2f1b6dde29e3f99e42c2df
Richard A <richard@mycloud.ee>
2020-05-28 23:43:49 +0300

    Fresh cookies from the oven

--- js/cookie.js
+++ js/cookie.js
@@ -2,8 +2,12 @@
 function cookieMan() {
     localStorage.setItem('RAPcookie', 'true');
     $('.cookies-alert').hide();
+    $('body').removeClass('cookie-padding');
 }
 
 if(localStorage.getItem('RAPcookie') == 'true') {
     $('.cookies-alert').hide();
+}
+else {
+    $('body').addClass('cookie-padding');
 }
